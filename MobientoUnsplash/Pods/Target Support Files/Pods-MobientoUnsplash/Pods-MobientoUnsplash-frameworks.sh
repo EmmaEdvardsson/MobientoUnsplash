b68024696c452e2ff7543f5a4f694a9640aa6cb6
EmmaEdvardsson <emma.edvardsson@frostdigital.se>
2018-04-05 10:38:22 +0200

    Adding RXSwift pod

--- MobientoUnsplash/Pods/Target Support Files/Pods-MobientoUnsplash/Pods-MobientoUnsplash-frameworks.sh
+++ MobientoUnsplash/Pods/Target Support Files/Pods-MobientoUnsplash/Pods-MobientoUnsplash-frameworks.sh
@@ -136,6 +136,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FXKeychain/FXKeychain.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SVProgressHUD/SVProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Unbox/Unbox.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Wrap/Wrap.framework"
@@ -143,6 +144,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FXKeychain/FXKeychain.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SVProgressHUD/SVProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Unbox/Unbox.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Wrap/Wrap.framework"
